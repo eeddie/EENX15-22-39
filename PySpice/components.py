3f73eb9a282861d82f51ec644ba131bbaf38af8d
Axel Henriksson <Axe.henriksson@gmail.com>
2022-02-15 23:18:21 +0100

    Blottade delta och lade till docstrings

--- PySpice/components.py
+++ PySpice/components.py
@@ -10,42 +10,54 @@ from PySpice.Unit import *
 from math import pi,atan,acos
 
 
-
-
 # Kontinuerliga spänningskällor
 # Desmos: https://www.desmos.com/calculator/578tvcfhoz
 # Källa: https://mathematica.stackexchange.com/questions/38293/make-a-differentiable-smooth-sawtooth-waveform
 
-# Lägger till en kontinuerlig triangelvågskälla till circuit som går från -amplitude till +amplitude
 # tri = 1 - 2 ArcCos[(1 - δ) Sin[2 π x]]/π;
-def contTriangleSource(circuit, name, in_node, out_node, frequency, amplitude, offset=0):
-    # delta ändrar den kontinuerliga triangelvågens skarphet. 0 ger perfekt (diskont.?) triangelvåg, 0.1 och högre ger sinusvåg.
-    delta = 0.0001
+def contTriangleSource(circuit, name, in_node, out_node, frequency, amplitude, offset=0, delta = 0.0001):
+    """
+    Lägger till en kontinuerlig triangelvågskälla till circuit som går från -amplitude till +amplitude
+
+    delta - ändrar den kontinuerliga triangelvågens skarphet. 0 ger perfekt (diskont.?) triangelvåg, 0.1 och högre ger sinusvåg.
+    """
+
     # factor ser till att signalens amplitud är 'amplitude' oavsett delta
     factor = 1 / float((1-(2/pi)*acos(1-delta)))
     period = 1 / float(frequency)
     circuit.BehavioralSource(name, in_node, out_node, v=f"{offset} + {amplitude * factor} * (1 - 2 * acos((1 - {delta}) * sin(2 * {pi} * (time / {period})))/{pi})")
 
-# Lägger till en kontinuerlig fyrkantsvågskälla till circuit som går från 0V till +amplitude
+
 #sqr 2 ArcTan[Sin[2 π x]/δ]/π;
-def contSquareSource(circuit, name, in_node, out_node, frequency, amplitude, offset=0):
-    # delta ändrar den kontinuerliga fyrkantsvågens skarphet. 0 ger perfekt (diskont.?) fyrkantsvåg, 0.1 och högre ger sinusvåg.
-    delta = 0.0001
+def contSquareSource(circuit, name, in_node, out_node, frequency, amplitude, offset=0, delta = 0.001):
+    """
+    Lägger till en kontinuerlig fyrkantsvågskälla till circuit som går från 0V till +amplitude
+
+    delta - ändrar den kontinuerliga fyrkantsvågens skarphet. 0 ger perfekt (diskont.?) fyrkantsvåg, 0.1 och högre ger sinusvåg.
+    """
+    
     # factor ser till att signalens amplitud är 'amplitude' oavsett delta
     factor = 1 / float(atan(1/(delta * pi)))
     period = 1 / float(frequency)
     circuit.BehavioralSource(name, in_node, out_node, v=f"{offset} + {amplitude} / 2 * (1 + {factor} * atan( sin({pi} * time / {period}) / {delta*pi}))")
 
-# Lägger till en kontinuerlig step-källa till circuit som stiger från 0V till +amplitude vid time = 0
-def contStepSource(circuit, name, in_node, out_node, amplitude):
-    # delta ändrar stegets skarphet. 0 ger ett perfekt (diskont.?) steg
-    delta = 0.000
+
+def contStepSource(circuit, name, in_node, out_node, amplitude, delta=0.001):
+    """ 
+    Lägger till en kontinuerlig step-källa till circuit som stiger från 0V till +amplitude vid time = 0
+
+    delta - ändrar stegets skarphet. 0 ger ett perfekt (diskont.?) steg
+    """
     circuit.BehavioralSource(name, in_node, out_node, v=f"{amplitude} * tanh(time / {delta})")
 
-# Lägger till en kontinuerlig pwm-källa till circuit som går från 0V till +amplitude
-def contPWMSource(circuit, name, in_node, out_node, frequency, amplitude, dutyCycle, offset=0):
-    # delta ändrar den kontinuerliga fyrkantsvågens skarphet. 0 ger perfekt (diskont.?) fyrkantsvåg, 0.1 och högre ger sinusvåg.
-    delta = 0.0001
+
+def contPWMSource(circuit, name, in_node, out_node, frequency, amplitude, dutyCycle, offset=0, delta = 0.001):
+    """
+    Lägger till en kontinuerlig pwm-källa till circuit som går från 0V till +amplitude
+    
+    delta - ändrar den kontinuerliga fyrkantsvågens skarphet. 0 ger perfekt (diskont.?) fyrkantsvåg, 0.1 och högre ger sinusvåg.
+    """
+    
     # factor ser till att signalens amplitud är 'amplitude' oavsett delta (trubbighet)
     factor = 1 / float(atan(1/(delta * pi)))
     period = 1 / float(frequency)
