835dd3264052a9739a499d1b7817d44e58edb5cc
Axel Henriksson <Axe.henriksson@gmail.com>
2022-02-16 17:30:44 +0100

    Gjorde om krets till funktion för parametrisering

--- PySpice/trefas.py
+++ PySpice/trefas.py
@@ -30,37 +30,21 @@ logger = Logging.setup_logging()
 #   └──────┴───────┴───────┘
 #    (circuit.gnd)
 
-# VARIABLER
-
-# Simuleringarnas tidssteg
-step_time = 1 @ u_us
-# Simuleringarnas sluttid
-final_time = 100 @ u_ms
-
-# Huvudsakliga frekvensen i trefassteget
-phaseFreq = 50 @ u_Hz
-# Fördröjningen mellan faserna (0°, 120°, 240°) i sekunder
-phaseDelay = (1 / phaseFreq) / 3 @ u_s
-
-# Triangelvågens frekvens i GHz
-triangleFreq = 450
-# Triangelvågens amplitud
-triangleAmplitude = 1
-
-# Sinussignalernas amplitud
-sinAmplitude = 700 @ u_mV
-
-# Batterispänning Vbat = batteryVoltage * tanh(batteryGain * time)
-# Enheter (ex. @ u_V) fungerar inte när man insertar i ett expression
-batteryVoltage = 1400       
-
-# Fasfilter "motor"
-phaseResistance = 1 @ u_Ohm
-phaseInductance = 1 @ u_mH
 
 
+def trefas(
+    phaseFreq = 50 @u_Hz,                  # Huvudsakliga frekvensen i trefassteget
+    triangleFreq = 450,                     # Triangelvågens frekvens i Hz
+    triangleAmplitude = 1,                  # Triangelvågens amplitud
+    sinAmplitude = 700 @u_mV,              # Sinussignalernas amplitud
+    batteryVoltage = 1400,                  # Batterispänning Vbat = batteryVoltage * tanh(batteryGain * time). Enheter (ex. @ u_V) fungerar inte när man insertar i ett expression
+    phaseResistance = 1 @u_Ohm,            # Fasresistans "motor"
+    phaseInductance = 1 @u_mH              # Fasinduktans "motor"
+):
+    """ Konstruerar en trefas-krets med de givna parametrarna och returnar den """
 
-if __name__ == '__main__':
+    phaseDelay = (1 / phaseFreq) / 3 @ u_s # Fördröjningen mellan faserna (0°, 120°, 240°) i sekunder
+
     circuit = Circuit('Tre-fas inverter')
 
     # Spänningskälla batteri
@@ -81,12 +65,26 @@ if __name__ == '__main__':
     circuit.R('Rd', 'c_c', circuit.gnd, 10 @ u_Ohm)
 
     # Komparatorer (sin > tri)
-    circuit.subcircuit(GreaterThanSubCircuit('sub_greaterThan'))
+    circuit.subcircuit(ContGreaterThanSubCircuit('sub_greaterThan', 1))
     circuit.X('gt_a', 'sub_greaterThan', 'a_a', 'check', 'control_a', circuit.gnd)
     circuit.X('gt_b', 'sub_greaterThan', 'b_b', 'check', 'control_b', circuit.gnd)
     circuit.X('gt_c', 'sub_greaterThan', 'c_c', 'check', 'control_c', circuit.gnd)
 
+
     # Fas-switchar
+
+    # IGBT:er importerade från Infineon. Ersätter switcharna längre ned, kräver annan gate-spänning (5V). Ger error just nu.
+    spice_library = SpiceLibrary('./libs/')
+    circuit.include(spice_library["IKW40N65H5A_L2"])
+    
+    # circuit.X('sw_a+', 'IKW40N65H5A_L2', 'Vbat', 'control_a', 'a')
+    # circuit.X('sw_b+', 'IKW40N65H5A_L2', 'Vbat', 'control_b', 'b')
+    # circuit.X('sw_c+', 'IKW40N65H5A_L2', 'Vbat', 'control_c', 'c')
+
+    # circuit.X('sw_a-', 'IKW40N65H5A_L2', 'a', 'control_a', circuit.gnd)
+    # circuit.X('sw_b-', 'IKW40N65H5A_L2', 'b', 'control_b', circuit.gnd)
+    # circuit.X('sw_c-', 'IKW40N65H5A_L2', 'c', 'control_c', circuit.gnd)
+
     circuit.subcircuit(SwitchSubCircuit('sub_switch'))
     circuit.X('sw_a+', 'sub_switch', 'Vbat', 'a', 'control_a', circuit.gnd)
     circuit.X('sw_b+', 'sub_switch', 'Vbat', 'b', 'control_b', circuit.gnd)
@@ -106,10 +104,30 @@ if __name__ == '__main__':
     circuit.L(3, 'c', 'cc', phaseInductance)
     circuit.R(3, 'cc', 'abc', phaseResistance)
 
+    return circuit
+
+
+
+
+
+# Simuleringarnas tidssteg
+step_time = 1 @ u_us
+# Simuleringarnas sluttid
+final_time = 100 @ u_ms
+
+# Fasresistansen som vi använder både i kretsen,, men också i simuleringen
+phaseResistance = 1 @u_Ohm
+
+if __name__ == '__main__':
+
+    circuit = trefas(phaseResistance = phaseResistance)
+
     # SIMULERING
     simulator = circuit.simulator(temperature=25, nominal_temperature=25)
     simulator.initial_condition(abc=0 @ u_V)
+
     analysis = simulator.transient(step_time=step_time, end_time=final_time)
+    
 
     # PLOTTING
     #
