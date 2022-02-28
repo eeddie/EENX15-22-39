00d1fb26ee61e0bfd3ef6f8db9845cda568e75dc
Eddie <eeddie@student.chalmers.se>
2022-02-28 10:25:05 +0100

    added comments

--- PySpice/simulate_netlist.py
+++ PySpice/simulate_netlist.py
@@ -9,6 +9,9 @@ def main():
         build_buck_netlist('BSB012N03LX3', '1u', '0.25', '33u', '0.5', '1.0Meg', str(duty), '0.25', 'BSB012N03LX3', '100', netlist_file)
         os.system(f'C:\\Spice64\\bin\\ngspice_con.exe -b -r {raw_file} -o {log_file} {netlist_file}')
 
+def example():
+    build_buck_netlist('BSB012N03LX3', '1u', '0.25', '33u', '0.5', '1.0Meg', '0.28', '0.25', 'BSB012N03LX3', '100', 'example.cir')
+
 
 if __name__ == '__main__':
     main()
