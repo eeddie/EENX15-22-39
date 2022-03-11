515d71f1962d7be7c31844da9faa2afa5f6ffb10
Axel Henriksson <Axe.henriksson@gmail.com>
2022-03-11 18:35:03 +0100

    Fourier transform in python

--- Modular/ParseRaw.py
+++ Modular/ParseRaw.py
@@ -1,11 +1,18 @@
 
+from cmath import pi
 import matplotlib.pyplot as plt
 
 import numpy as np
 
-from scipy import fft
+from scipy import signal
+from scipy.fftpack import fft, fftfreq
+from scipy.signal import TransferFunction
 
 from ltspice import Ltspice
+
+# Non uniform fourier transform, for transforms with uneven time steps
+# NUDFT is supposedly the same but with double precision.
+#from pynufft import NUFFT, NUDFT
 
 
 if __name__ == "__main__":
@@ -19,12 +26,30 @@ if __name__ == "__main__":
     timeVec = l.get_time()
     currentVec = l.get_data("i(l.xload.l1)")
 
+    # Plot current over time
     plt.figure(0)
     plt.plot(timeVec, currentVec)
 
-    # Plot timestep size
+    # Plot timestep size, shows a constant line at the set timestep with dips when the simulator decreases the timestep
     #diffTime = timeVec[1:timeVec.size-1] - timeVec[0:timeVec.size-2]
     #plt.figure(1)
     #plt.plot(timeVec[0:timeVec.size-2], diffTime)
 
+
+    # Plot fourier of current   NOTE:   Time vector is non-uniform, but fft can only be done on uniform data, thus this is slightly inaccurate
+    #                                   PyNUFFT package is for non-uniform fft but I could not get it to work.
+    fourier = fft(currentVec)
+
+    plt.figure(1)
+    # Number of sample points
+    N = timeVec.size
+    # sample spacing
+    T = 5*(10**-9)
+    yf = fft(currentVec)
+    xf = fftfreq(N, T)[:N//2]
+
+    plt.yscale("log")
+    plt.xscale("log")
+    plt.plot(xf, 2.0/N * np.abs(yf[0:N//2]))
+    plt.grid()
     plt.show()
