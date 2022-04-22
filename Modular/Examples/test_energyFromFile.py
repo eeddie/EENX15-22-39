7f579c8e64d6ab227c160d1a0ca46e22c693983b
Eddie <eeddie@student.chalmers.se>
2022-04-22 10:56:16 +0200

    Edit test function for compatibility

--- Modular/Examples/test_energyFromFile.py
+++ Modular/Examples/test_energyFromFile.py
@@ -7,14 +7,14 @@ def test_energyFromFile():
     variables = ["i(vac_a)", "i(vac_b)", "i(vdc_p)"]
     energies = energyFromFile("test.raw", *variables)
     # [time, data] = readVariables("test.raw","i(vac_a)", "i(vac_b)", "i(vdc_p)")
-    freq = [row[0] for row in phaseEnergy]
-    value = [row[2]/row[3] for row in phaseEnergy]
+    freq = [row[0] for row in energies]
+    value = [row[3]/row[2] for row in energies]
     plt.plot(freq,value,'-', linewidth=1,alpha=0.5, label="i(phA) avg:d")
 
-    phaseEnergy1 = energies["i(vdc_p)"]
-    freq1 = [row[0] for row in phaseEnergy1]
-    value1 = [row[2]/row[3] for row in phaseEnergy1]
-    plt.plot(freq1,value1,'-', linewidth=1,alpha=0.5, label="i(dc_p) avg:d")
+    # phaseEnergy1 = energies["i(vdc_p)"]
+    # freq1 = [row[0] for row in phaseEnergy1]
+    # value1 = [row[2]/row[3] for row in phaseEnergy1]
+    # plt.plot(freq1,value1,'-', linewidth=1,alpha=0.5, label="i(dc_p) avg:d")
     plt.legend()
     plt.loglog()
     plt.grid()
