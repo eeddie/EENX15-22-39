c639459c4a2cab6579522a3fd05dcf570da34309
Axel Henriksson <Axe.henriksson@gmail.com>
2022-04-28 16:51:36 +0200

    Ny FFTRaw

--- Modular/FFTRaw.py
+++ Modular/FFTRaw.py
@@ -1,9 +1,7 @@
-import json
 from typing import Any
+from Functions import *
+from Bfalt import getBfaltAmplitude
 
-from ltspice import FileSizeNotMatchException
-
-from Simulate import *
 import sys
 
 
@@ -11,7 +9,8 @@ def np_encoder(obj):
     if isinstance(obj, np.generic):
         return obj.item()
 
-def energyFromFile(filename: str, dcI: list[str], *variables: str):
+
+def energyAndBfaltFromFile(filename: str, dcI: list[str], *variables: str):
     [time, data] = readVariables(filename, *variables)
     uniVariables = {}
     uniTime: Any
@@ -22,45 +21,15 @@ def energyFromFile(filename: str, dcI: list[str], *variables: str):
     N = len(uniTime)
     tf = fftfreq(N, uniTime[1] - uniTime[0])[0:N // 2]
     yfs = [2.0 / N * np.abs(fft(uniVariables[var])[0:N // 2]) for var in variables]
+    Bfalt = BfaltFromFile(filename, dcI[0], dcI[1])
 
-    energy = energyInAllBands(tf, *yfs)
-    Bfalt = processedEnergyFromFile(filename, dcI[0], dcI[1])
-
-    assert len(energy) == len(Bfalt)
-    for i in range(len(energy)):
-        energy[i].append(Bfalt[i])
+    energy = energyInAllBands(tf,*yfs, *Bfalt)
 
     return json.dumps(energy, default=np_encoder)
 
 
-def energyInDCBands(xf, *yf):
-    limits = [0, 10000, 150000, 30000000, 400000000]
-    bandwidths = [100 * 10 ** p for p in range(5)]
-    frequencies = []
-    for i in range(len(limits) - 1):
-        for j in range(limits[i], limits[i + 1], bandwidths[i]):
-            frequencies.append(j)
-    frequencies.append(limits[-1])
 
-    energy = []
-    for index, startFreq in enumerate(frequencies):
-        if startFreq == frequencies[-1]:
-            break
-        endFreq = frequencies[index + 1]
-
-        lo = find_nearest_frequency(array=xf, value=startFreq)
-        hi = find_nearest_frequency(array=xf, value=endFreq)
-
-        bandEnergy = [sum_energy(yf=entry, lower=lo, upper=hi) for entry in yf]
-
-        energy.append([*bandEnergy])
-    return energy
-
-
-from Bfalt import getBfaltAmplitude
-
-
-def processedEnergyFromFile(filename: str, *variables: str):
+def BfaltFromFile(filename: str, *variables: str):
     [time, data] = readVariables(filename, *variables)
     uniVariables = {}
     uniTime: Any
@@ -70,12 +39,12 @@ def processedEnergyFromFile(filename: str, *variables: str):
 
     N = len(uniTime)
     tf = fftfreq(N, uniTime[1] - uniTime[0])[0:N // 2]
-    yfs = [2.0 / N * np.abs(fft(uniVariables[var])[0:N // 2]) for var in variables]
+    yfs = [2.0 / N * fft(uniVariables[var])[0:N // 2] for var in variables]
 
-    return getBfaltAmplitude(energyInDCBands(tf, *yfs)).tolist()
+    return getBfaltAmplitude(yfs[0], yfs[1])
 
 
-def saveModifiedSim(filename: str, modules: list, simParams: dict, results: dict):
+def saveModifiedSim(filename: str, modules: list, simParams: dict, variables:list, results: dict):
     """ Sparar ned simuleringens parametrar till en JSON-fil, lägger till simuleringen om filen redan existerar """
 
     with open(filename, "w+") as file:  # Öppna/Skapa json fil
@@ -89,6 +58,7 @@ def saveModifiedSim(filename: str, modules: list, simParams: dict, results: dict
                 "modules": modules,
                 # "modules" är en dict med moduler där modulnamn är key och parameterdicten är value
                 "simParams": simParams,  # "simParams" är en dict med simuleringsparametrar ex. tstep, tstart, tstop
+                "variables":variables,
                 "results": results  # Results är en dict med resultat. Ex. {"commonModeCurrent": 50}
             })
         file.seek(0)  # Börja om filen från början så vi skriver över filen med den nya datan
@@ -96,24 +66,30 @@ def saveModifiedSim(filename: str, modules: list, simParams: dict, results: dict
 
 
 if __name__ == "__main__":
-    parameterFile = "simResults\\params" + str(sys.argv[1]) + ".json"
+    parameterFile = "simResults/params" + str(sys.argv[1]) + ".json"
     simFile = "sim" + str(sys.argv[1]) + ".raw"
+    logFile = "sim" + str(sys.argv[1]) + ".log"
 
     with open(parameterFile) as f:
         data = json.load(f)
 
+    variables = ["i(l.xload.l1)", "i(VDC_P)", "i(VDC_N)"]
+    failed = False
     result: Any
     try:
-        result = energyFromFile(simFile,["i(VDC_P)", "i(VDC_N)"], "i(l.xload.l1)","i(VDC_P)", "i(VDC_N)")
-    except Exception and UnicodeDecodeError:
-        result = None
+        result = energyAndBfaltFromFile(simFile, ["i(VDC_P)", "i(VDC_N)"], *variables)
+    except Exception as e:
+        failed = True
+        print(e)
+        result = open(logFile, "r").readlines()
+
     saveModifiedSim("simResults\\sim" + str(sys.argv[1]) + ".json",
                     modules=data[0]["modules"],
                     simParams=data[0]["simParams"],
-                    results={
-                        "energies": result
-                    }
+                    variables = variables,
+                    results={"energies": result} if not failed else {"log": result}
                     )
 
+    os.remove(logFile)
     os.remove(parameterFile)
     os.remove(simFile)
